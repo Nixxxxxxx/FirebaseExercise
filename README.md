87dfb9ea01c5ad2c1ac73c64893793f3068f8762
Nissen Parianen <n.parianen@gmail.com>
2017-02-01 14:18:17 +0100

    Added gradle dependencies for firebase

---
+++ README.md
@@ -0,0 +1,5 @@
+TODO
+    google sign in
+    use google credential for email authetication
+    add exercise selection activity
+    add UML
---
+++ app/src/main/java/german/teach/learn/zero/learnandteachgerman/ExerciseSelection.java
@@ -0,0 +1,13 @@
+package german.teach.learn.zero.learnandteachgerman;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExerciseSelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise_selection);
+    }
+}
