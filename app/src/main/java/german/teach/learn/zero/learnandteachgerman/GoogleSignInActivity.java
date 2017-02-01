89197f44752fc999f0073ae1315d9049fb56841f
Nissen Parianen <n.parianen@gmail.com>
2017-02-01 14:08:21 +0100

    Initial commit

---
+++ app/src/main/java/german/teach/learn/zero/learnandteachgerman/GoogleSignInActivity.java
@@ -0,0 +1,13 @@
+package german.teach.learn.zero.learnandteachgerman;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GoogleSignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_sign_in);
+    }
+}
