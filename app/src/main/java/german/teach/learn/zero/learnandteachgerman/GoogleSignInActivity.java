d6bd167c87f82d45f0319d13431f4337fec4ceea
Nissen Parianen <n.parianen@gmail.com>
2017-02-01 14:30:42 +0100

    Added basic google account sign in

--- app/src/main/java/german/teach/learn/zero/learnandteachgerman/GoogleSignInActivity.java
+++ app/src/main/java/german/teach/learn/zero/learnandteachgerman/GoogleSignInActivity.java
@@ -1,13 +1,119 @@
 package german.teach.learn.zero.learnandteachgerman;
 
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class GoogleSignInActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+
+public class GoogleSignInActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
+    SignInButton mSignInButton;
+    Button mSignOutButton;
+    TextView mStatusTextView;
+    GoogleApiClient mGoogleApiClient;
+
+    private static final String TAG = "SingInActivity";
+    private static final int RC_SIGN_IN = 10001;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google_sign_in);
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this) //this.activity, this.onClick
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+        mStatusTextView = (TextView) findViewById(R.id.status_textview);
+        mStatusTextView.setText("xxxxxxxx");
+
+        mSignInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        mSignInButton.setOnClickListener(this);
+
+        mSignOutButton = (Button) findViewById(R.id.sign_out_button);
+        mSignOutButton.setOnClickListener(this);
+
     }
-}
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.sign_in_button:
+                signIn();
+                break;
+            case R.id.sign_out_button:
+                signOut();
+                break;
+        }
+    }
+
+    public void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult");
+        if (result.isSuccess()) {
+            GoogleSignInAccount acct = result.getSignInAccount();
+            mStatusTextView.setText("Hallo " + acct.getDisplayName());
+            startActivity(new Intent(GoogleSignInActivity.this, ExerciseSelection.class));
+        } else {
+
+        }
+    }
+
+
+    public void signOut() {
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(@NonNull Status status) {
+                mStatusTextView.setText("Signed Out");
+            }
+        });
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d(TAG, "onConnectionFailed " + connectionResult);
+
+    }
+}//END OF CLASS
+
+
+
+
+
+
